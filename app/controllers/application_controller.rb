9b36b9b377897d9ba5a3f3e96c0307b83d39d089
Sophia Davis <sophia@Sophias-MacBook-Air.local>
2013-06-17 16:27:40 -0500

    redirect changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+	protect_from_forgery
+	before_filter :ensure_domain
+	
+	def ensure_domain
+		canonical_domain = 'tweetvirgin.com'
+		
+		if request.host != canonical_domain && ENV["RAILS_ENV"] == 'production'
+			redirect_to request.protocol + canonical_domain + request.request_uri
+		end
+	end
 end
