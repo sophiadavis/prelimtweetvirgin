b8e8ef42bcba48e82ab2794da97db1347a17079e
Sophia Davis <sophia@Sophias-MacBook-Air.local>
2013-06-18 09:51:40 -0500

    fixing redirects II

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
 	protect_from_forgery
 
 	before_filter :ensure_domain
-
+=begin
 	APP_DOMAIN = 'www.tweetvirgin.com'
 
 	def ensure_domain
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
 		  redirect_to "http://#{APP_DOMAIN}#{request.env['REQUEST_PATH']}", :status => 301
 		end
 	end
-
+=end
 =begin	
 	before_filter :ensure_domain
 	
@@ -21,15 +21,12 @@ class ApplicationController < ActionController::Base
 	
 	end
 =end
-	
-=begin	
 	def ensure_domain
 		canonical_domain = 'www.tweetvirgin.com'
-		debugger
-		logger.info request.inspect
+		#debugger
+		#logger.info request.inspect
 		if request.host != canonical_domain && ENV["RAILS_ENV"] == 'production'
 			redirect_to request.protocol + canonical_domain + request.request_uri
 		end
 	end
-=end
 end
