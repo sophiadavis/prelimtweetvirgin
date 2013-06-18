1788c730636413cad4a4a9ad8d7bd59372a1d191
Sophia Davis <sophia@Sophias-MacBook-Air.local>
2013-06-18 15:24:36 -0500

    redirect w middleware

--- lib/middleware/www_middleware.rb
+++ lib/middleware/www_middleware.rb
@@ -17,7 +17,7 @@ class WwwMiddleware
 		#[200, {"Content-Type" => "text/html"}, "Hello, World!"]
 =end
 		request = Rack::Request.new(env)
-		logger.info request.inspect
+		#logger.info request.inspect
 		if request.host.starts_with?("*.com")
 			[301, {"Location" => request.url.sub("//*.", "//www.*.com")}, self]
 
