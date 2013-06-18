decf7db8a31300e39332be4d685611241d3b7e7a
Sophia Davis <sophia@Sophias-MacBook-Air.local>
2013-06-18 14:41:57 -0500

    redirect w middleware

--- lib/middleware/www_middleware.rb
+++ lib/middleware/www_middleware.rb
@@ -5,12 +5,20 @@ class WwwMiddleware
 	end
 
 	def call(env)
+		request = Rack::Request.new(env)
+		
+		unless request.ssl?
+			url = request.url.sub(“http://&#8221;,”https://&#8221;)
+			url = url.sub(“//tweetvirgin.com”, “//www.tweetvirgin.com”)
+			[301, {"Location" => url}, self]
+=begin		
 		#[200, {"Content-Type" => "text/html"}, "Hello, World!"]
 
 		request = Rack::Request.new(env)
 	
 		if request.host.starts_with?("*.com")
 			[301, {"Location" => request.url.sub("//*.", "//www.*.com")}, self]
+=end
 		else
 			@app.call(env)
 		end
