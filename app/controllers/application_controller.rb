721d4f8e2f96e3f609d935e84a0c15ec3cbc2602
Sophia Davis <sophia@Sophias-MacBook-Air.local>
2013-06-18 11:57:03 -0500

    more redirecting?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,20 @@
 class ApplicationController < ActionController::Base
 	protect_from_forgery
 
-	before_filter :ensure_domain
+before_filter :force_www!
+	
+	protected
+	
+	def force_www!
+		if Rails.env.production? and request.host[0..3] != “www.”
+		redirect_to “#{request.protocol}www.#{request.host_with_port}#{request.fullpath}”, :status => 301
+		end
+	end
+
+
 =begin
+	before_filter :ensure_domain
+
 	APP_DOMAIN = 'www.tweetvirgin.com'
 
 	def ensure_domain
@@ -11,8 +23,7 @@ class ApplicationController < ActionController::Base
 		  redirect_to "http://#{APP_DOMAIN}#{request.env['REQUEST_PATH']}", :status => 301
 		end
 	end
-=end
-=begin	
+	
 	before_filter :ensure_domain
 	
 	def ensure_domain
@@ -20,7 +31,7 @@ class ApplicationController < ActionController::Base
 		redirect_to request.protocol + "www." + request.host_with_port + request.request_uri if !/^www/.match(request.host) if Rails.env == 'production'
 	
 	end
-=end
+
 	def ensure_domain
 		canonical_domain = 'www.tweetvirgin.com'
 		#debugger
@@ -29,6 +40,7 @@ class ApplicationController < ActionController::Base
 			redirect_to request.protocol + canonical_domain + request.fullpath
 		end
 	end
+=end
 end
 # request_uri
 # request_url
