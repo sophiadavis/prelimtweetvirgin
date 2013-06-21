5dcd587e5ea9972680ae155536d42e31cf2099a3
Sophia Davis <sophia@Sophias-MacBook-Air.local>
2013-06-21 14:27:24 -0500

    messing with js

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,2 +1,3 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+document.write("<h1>This is a heading.<h1>")
